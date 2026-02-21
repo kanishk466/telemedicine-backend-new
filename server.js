325fbd3181cc5585ed19f2aed77b96750c509b50
Kanishk Singh Maurya <95866249+kanishk466@users.noreply.github.com>
2026-02-21 12:40:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,4 @@ import app from "./src/app.js";
 import ngrok from "@ngrok/ngrok";
 app.listen(process.env.PORT,async () => {
   console.log("Server running on port 8800");
-    try {
-    const listener = await ngrok.connect({
-      addr: process.env.PORT,
-      authtoken: process.env.NGROK_AUTHTOKEN,
-      domain: "arlette-uniconoclastic-juanita.ngrok-free.app",
-    });
-    console.log(`Public URL: ${listener.url()}`);
-  } catch (err) {
-    console.error("Failed to start ngrok:", err.message);
-  }
 });
