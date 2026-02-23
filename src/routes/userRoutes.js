cf77c8afe1e744bca91c2f3688cf27cc7d352d61
Kanishk Singh Maurya <95866249+kanishk466@users.noreply.github.com>
2026-02-23 14:36:29 +0530

    Add authentication routes for register and login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { getDoctorsByClinic } from "../controllers/UserController.js";
+import { register , login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 // router.get("/doctors/:clinicId/:searchQuery", getDoctorsByClinic);
-
+router.post("/auth/register", register);
+router.post("/auth/login", login);
 router.get("/clinics/:clinicId/:role", getDoctorsByClinic);
 
 export default router;
+
